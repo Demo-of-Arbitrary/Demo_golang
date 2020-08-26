01f8519e272e74124949407d11687d7aefca2a59
George Xie <georgexie@George.local>
2020-08-26 15:06:13 +0800

    feat: new function sumAll

--- learn_go_with_tests/array/sum.go
+++ learn_go_with_tests/array/sum.go
@@ -1,9 +1,18 @@
 package arrays
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	var sum int
 	for _, number := range numbers {
 		sum += number
 	}
+	return sum
+}
+
+func SumAll(numbers []int, numbers2 []int) [2]int {
+	var sum [2]int
+	sum1 := Sum(numbers)
+	sum2 := Sum(numbers2)
+	sum[0] = sum1
+	sum[1] = sum2
 	return sum
 }
--- learn_go_with_tests/array/sum_test.go
+++ learn_go_with_tests/array/sum_test.go
@@ -1,14 +1,28 @@
 package arrays
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-	t.Run("calculate sum", func(t *testing.T) {
-		numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("calculate any size of numbers' sum", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
 		result := Sum(numbers)
-		expected := 15
+		expected := 6
 		if result != expected {
 			t.Errorf("expected %d, got %d", expected, result)
+		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	t.Run("calculate collective array", func(t *testing.T) {
+		numbers1 := []int{1, 2, 3, 4}
+		numbers2 := []int{1, 2, 3, 4, 5, 6}
+		result := SumAll(numbers1, numbers2)
+		expected := [2]int{10, 21}
+		if result != expected {
+			t.Errorf("expected %+v, got %+v", expected, result)
 		}
 	})
 }
