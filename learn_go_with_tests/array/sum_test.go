ee809756c6c4a6cc7f03348a2834adf574e4e8c2
George Xie <georgexie@George.local>
2020-08-26 14:42:16 +0800

    feat: array example

---
+++ learn_go_with_tests/array/sum_test.go
@@ -0,0 +1,14 @@
+package arrays
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	t.Run("calculate sum", func(t *testing.T) {
+		numbers := [5]int{1, 2, 3, 4, 5}
+		result := Sum(numbers)
+		expected := 15
+		if result != expected {
+			t.Errorf("expected %d, got %d", expected, result)
+		}
+	})
+}
