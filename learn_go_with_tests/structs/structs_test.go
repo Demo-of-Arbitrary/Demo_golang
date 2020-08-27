fb88100f1985eec47c31e1423ac3c10d11604914
George Xie <georgexie@George.local>
2020-08-27 22:29:06 +0800

    feat: support calculate area of circle

--- learn_go_with_tests/structs/structs_test.go
+++ learn_go_with_tests/structs/structs_test.go
@@ -12,10 +12,22 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	reactAngle := Rectangle{8.0, 2.0}
-	actual := Area(reactAngle)
-	expected := 16.0
-	if actual != expected {
-		t.Errorf("expected %.2f, got %.2f", expected, actual)
-	}
+	t.Run("rectangles", func(t *testing.T) {
+		rectAngle := Rectangle{8.0, 2.0}
+		actual := rectAngle.Area()
+		expected := 16.0
+		if actual != expected {
+			t.Errorf("expected %.2f, got %.2f", expected, actual)
+		}
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		actual := circle.Area()
+		expected := 314.1592653589793
+
+		if actual != expected {
+			t.Errorf("expected %g, got %g", expected, actual)
+		}
+	})
 }
