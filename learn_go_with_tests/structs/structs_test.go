80a076d20aa40c474d99eba297789b53f904c32a
George Xie <georgexie@George.local>
2020-08-27 22:36:09 +0800

    refact: with interface

--- learn_go_with_tests/structs/structs_test.go
+++ learn_go_with_tests/structs/structs_test.go
@@ -12,22 +12,23 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
+	checkArea := func(t *testing.T, shape Shape, expected float64) {
+		t.Helper()
+		actual := shape.Area()
+		if actual != expected {
+			t.Errorf("expected %f, got %f", expected, actual)
+		}
+	}
 	t.Run("rectangles", func(t *testing.T) {
 		rectAngle := Rectangle{8.0, 2.0}
-		actual := rectAngle.Area()
 		expected := 16.0
-		if actual != expected {
-			t.Errorf("expected %.2f, got %.2f", expected, actual)
-		}
+		checkArea(t, rectAngle, expected)
 	})
 
 	t.Run("circles", func(t *testing.T) {
 		circle := Circle{10}
-		actual := circle.Area()
 		expected := 314.1592653589793
 
-		if actual != expected {
-			t.Errorf("expected %g, got %g", expected, actual)
-		}
+		checkArea(t, circle, expected)
 	})
 }
