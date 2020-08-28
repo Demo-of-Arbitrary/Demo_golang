b14b981372564f46eef87493245fabce0bca0986
George Xie <georgexie@George.local>
2020-08-28 17:24:12 +0800

    fix: using pointer to fix

--- learn_go_with_tests/pointer/pointer.go
+++ learn_go_with_tests/pointer/pointer.go
@@ -6,11 +6,11 @@ type Wallet struct {
 	balance int
 }
 
-func (w Wallet) Balance() int {
+func (w *Wallet) Balance() int {
 	return w.balance
 }
 
-func (w Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount int) {
 	w.balance += amount
 	fmt.Printf("address of package is %v \n", &w.balance)
 }
