8d8fa16839dc7c4b04c188509d78c378f7b8b63c
George Xie <georgexie@George.local>
2020-08-28 17:23:40 +0800

    feat: wallet before use pointer

---
+++ learn_go_with_tests/pointer/pointer.go
@@ -0,0 +1,16 @@
+package pointer
+
+import "fmt"
+
+type Wallet struct {
+	balance int
+}
+
+func (w Wallet) Balance() int {
+	return w.balance
+}
+
+func (w Wallet) Deposit(amount int) {
+	w.balance += amount
+	fmt.Printf("address of package is %v \n", &w.balance)
+}
---
+++ learn_go_with_tests/pointer/pointer_test.go
@@ -0,0 +1,20 @@
+package pointer
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestWallet(t *testing.T) {
+	wallet := Wallet{}
+	wallet.Deposit(10)
+	got := wallet.Balance()
+
+	fmt.Printf("address of test is %v \n", &wallet.balance)
+
+	want := 10
+
+	if got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+}
