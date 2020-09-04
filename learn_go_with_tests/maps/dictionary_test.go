c94db9bcfd02c153242a03336e3fffe8c9a81f29
George Xie <georgexie@George.local>
2020-09-04 21:38:03 +0800

    feat: support add word to dictionary

--- learn_go_with_tests/maps/dictionary_test.go
+++ learn_go_with_tests/maps/dictionary_test.go
@@ -8,6 +8,12 @@ func assertString(t *testing.T, want string, got string) {
 		t.Errorf("want %q, got %q", want, got)
 	}
 }
+func assertError(t *testing.T, want, got error) {
+	t.Helper()
+	if got != want {
+		t.Errorf("want %q, got %q", want, got)
+	}
+}
 
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
@@ -20,11 +26,27 @@ func TestSearch(t *testing.T) {
 
 	t.Run("unknown Word", func(t *testing.T) {
 		_, err := dictionary.Search("good")
-		want := "could not find the word you were looking for"
+		want := ErrorNotFound
 		if err == nil {
 			t.Fatal("expected to get an error")
 		}
 
-		assertString(t, err.Error(), want)
+		assertError(t, err, want)
+	})
+
+}
+
+func TestAdd(t *testing.T) {
+	t.Run("add word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		dictionary.Add("apple", "this is a fruit")
+		got, err := dictionary.Search("apple")
+
+		if err != nil {
+			t.Fatal("should find added word", err)
+		}
+
+		want := "this is a fruit"
+		assertString(t, want, got)
 	})
 }
