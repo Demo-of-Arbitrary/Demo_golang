749d0cded0f6a7979a4102f64bfbe239ba166637
George Xie <georgexie@George.local>
2020-09-07 16:00:37 +0800

    feat: implement ohters and inject dependecny of sleep

--- learn_go_with_tests/mocking/countdown.go
+++ learn_go_with_tests/mocking/countdown.go
@@ -4,12 +4,41 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
-func Countdown(b io.Writer) {
-	fmt.Fprintf(b, "3")
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+const countDownNum = 3
+const finalValue = "Go!"
+
+func Countdown(b io.Writer, sleeper Sleeper) {
+	for i := countDownNum; i > 0; i-- {
+		sleeper.Sleep()
+		fmt.Fprintln(b, i)
+	}
+
+	sleeper.Sleep()
+	fmt.Fprint(b, finalValue)
 }
 
 func main() {
-	Countdown(os.Stdout)
+	sleeper := DefaultSleeper{}
+	Countdown(os.Stdout, &sleeper)
 }
