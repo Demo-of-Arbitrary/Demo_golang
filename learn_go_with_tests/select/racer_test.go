b80379b970857870db48deed240258c465a645d0
George Xie <georgexie@George.local>
2020-09-08 16:00:25 +0800

    feat: implement timeout

--- learn_go_with_tests/select/racer_test.go
+++ learn_go_with_tests/select/racer_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestRacer(t *testing.T) {
-	t.Run("Racer", func(t *testing.T) {
+	t.Run("get the faster one", func(t *testing.T) {
 		slowServer := makeDelayedServer(20)
 		fastServer := makeDelayedServer(0)
 
@@ -19,18 +19,32 @@ func TestRacer(t *testing.T) {
 		fastUrl := fastServer.URL
 
 		want := fastUrl
-		got := Racer(slowUrl, fastUrl)
+		got, _ := Racer(slowUrl, fastUrl)
 
 		if got != want {
 			t.Errorf("got %q, want %q", got, want)
 		}
 
 	})
+
+	t.Run("returns an error if doesn't respond within 10s", func(t *testing.T) {
+		serverA := makeDelayedServer(11 * time.Second)
+		serverB := makeDelayedServer(12 * time.Second)
+
+		defer serverA.Close()
+		defer serverB.Close()
+
+		_, err := Racer(serverA.URL, serverB.URL)
+
+		if err == nil {
+			t.Errorf("expected an error but didn't get one'")
+		}
+	})
 }
 
 func makeDelayedServer(delay time.Duration) *httptest.Server {
 	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(delay * time.Millisecond)
+		time.Sleep(delay)
 		w.WriteHeader(http.StatusOK)
 	}))
 }
