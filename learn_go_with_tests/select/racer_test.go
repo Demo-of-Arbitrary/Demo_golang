89535ffac201f083063c44f014df6755eab546ea
George Xie <georgexie@George.local>
2020-09-08 15:41:46 +0800

    refact: extract; defer usage; httptest usage

--- learn_go_with_tests/select/racer_test.go
+++ learn_go_with_tests/select/racer_test.go
@@ -1,11 +1,22 @@
 package racer
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 func TestRacer(t *testing.T) {
 	t.Run("Racer", func(t *testing.T) {
-		const slowUrl = "http://www.facebook.com"
-		const fastUrl = "http://www.quii.co.uk"
+		slowServer := makeDelayedServer(20)
+		fastServer := makeDelayedServer(0)
+
+		defer slowServer.Close()
+		defer fastServer.Close()
+
+		slowUrl := slowServer.URL
+		fastUrl := fastServer.URL
 
 		want := fastUrl
 		got := Racer(slowUrl, fastUrl)
@@ -13,5 +24,13 @@ func TestRacer(t *testing.T) {
 		if got != want {
 			t.Errorf("got %q, want %q", got, want)
 		}
+
 	})
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
 }
