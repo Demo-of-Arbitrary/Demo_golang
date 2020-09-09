693d7df6029758085c0ac3ae11114010a3caef88
George Xie <georgexie@George.local>
2020-09-09 15:31:55 +0800

    refact: extract common code
    Signed-off-by: George Xie <georgexie@George.local>

--- learn_go_with_tests/reflection/reflection.go
+++ learn_go_with_tests/reflection/reflection.go
@@ -5,27 +5,25 @@ import "reflect"
 func walk(x interface{}, fn func(input string)) {
 	val := getValue(x)
 
-	numberOfValues := 0
-
-	var getField func(int) reflect.Value
+	walkValue := func(value reflect.Value) {
+		walk(value.Interface(), fn)
+	}
 
 	switch val.Kind() {
 	case reflect.Struct:
-		numberOfValues = val.NumField()
-		getField = val.Field
+		for i := 0; i < val.NumField(); i++ {
+			walkValue(val.Field(i))
+		}
 	case reflect.Slice, reflect.Array:
-		numberOfValues = val.Len()
-		getField = val.Index
+		for i := 0; i < val.Len(); i++ {
+			walkValue(val.Index(i))
+		}
 	case reflect.Map:
 		for _, k := range val.MapKeys() {
-			walk(val.MapIndex(k).Interface(), fn)
+			walkValue(val.MapIndex(k))
 		}
 	case reflect.String:
 		fn(val.String())
-	}
-
-	for i := 0; i < numberOfValues; i++ {
-		walk(getField(i).Interface(), fn)
 	}
 }
 
