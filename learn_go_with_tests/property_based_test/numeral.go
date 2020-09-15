dcd520c744bb75f7b7c85fa6437dd1fac966908f
George Xie <georgexie@George.local>
2020-09-15 10:57:22 +0800

    refact: first refeactor

--- learn_go_with_tests/property_based_test/numeral.go
+++ learn_go_with_tests/property_based_test/numeral.go
@@ -1,11 +1,20 @@
 package numeral
 
+import "strings"
+
 func ConvertToRoman(arabic int) string {
-	if arabic == 2 {
-		return "II"
+	var result strings.Builder
+
+	for i := arabic; i > 0; i-- {
+		if i == 5 {
+			result.WriteString("V")
+			break
+		}
+		if i == 4 {
+			result.WriteString("IV")
+			break
+		}
+		result.WriteString("I")
 	}
-	if arabic == 3 {
-		return "III"
-	}
-	return "I"
+	return result.String()
 }
