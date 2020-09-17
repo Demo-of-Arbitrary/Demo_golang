81d5e6642c12beb6bf3b568721203b1be6237cee
George Xie <georgexie@George.local>
2020-09-17 11:00:31 +0800

    refact: transit to table test

--- learn_go_with_tests/maths/clockface_test.go
+++ learn_go_with_tests/maths/clockface_test.go
@@ -56,21 +56,42 @@ type Line struct {
 // }
 
 func TestSVGWriterAtMidnight(t *testing.T) {
-	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
-
-	b := bytes.Buffer{}
-	clockface.SVGWriter(&b, tm)
-
-	svg := SVG{}
-	xml.Unmarshal(b.Bytes(), &svg)
-
-	want := Line{150, 150, 150, 60}
-
-	for _, line := range svg.Line {
-		if line == want {
-			return
-		}
+	cases := []struct {
+		time time.Time
+		line Line
+	}{
+		{simpleTime(0, 0, 0), Line{150, 150, 150, 60}},
+		{simpleTime(0, 0, 30), Line{150, 150, 150, 240}},
 	}
 
-	t.Errorf("Expected to find the second hand line %+v, in the SVG lines %+v", want, svg.Line)
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+
+			b := bytes.Buffer{}
+			clockface.SVGWriter(&b, c.time)
+			svg := SVG{}
+			xml.Unmarshal(b.Bytes(), &svg)
+
+			if !containsLine(c.line, svg.Line) {
+
+				t.Errorf("Expected to find the second hand line %+v, in the SVG lines %+v", c.line, svg.Line)
+			}
+		})
+	}
+}
+
+func containsLine(c Line, s []Line) bool {
+	for _, l := range s {
+		if l == c {
+			return true
+		}
+	}
+	return false
+}
+
+func testName(t time.Time) string {
+	return t.Format("15:04:05")
+}
+func simpleTime(hours, minutes, seconds int) time.Time {
+	return time.Date(312, time.October, 28, hours, minutes, seconds, 0, time.UTC)
 }
