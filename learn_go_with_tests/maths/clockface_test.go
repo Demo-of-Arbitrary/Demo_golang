d1c93cd2580a78193cbdee8cc7132d88f52586cf
George Xie <georgexie@George.local>
2020-09-18 14:30:00 +0800

    refact: adjust file structure

--- learn_go_with_tests/maths/clockface_test.go
+++ learn_go_with_tests/maths/clockface_test.go
@@ -1,144 +1,148 @@
-package clockface_test
+package clockface
 
 import (
-	"bytes"
-	"encoding/xml"
+	"math"
 	"testing"
 	"time"
-
-	clockface "github.com/Demo_golang/learn_go_with_tests/maths"
 )
 
-type SVG struct {
-	XMLName xml.Name `xml:"svg"`
-	Xmlns   string   `xml:"xmlns,attr"`
-	Width   string   `xml:"width,attr"`
-	Height  string   `xml:"height,attr"`
-	ViewBox string   `xml:"viewBox,attr"`
-	Version string   `xml:"version,attr"`
-	Circle  Circle   `xml:"circle"`
-	Line    []Line   `xml:"line"`
-}
-
-type Circle struct {
-	Cx float64 `xml:"cx,attr"`
-	Cy float64 `xml:"cy,attr"`
-	R  float64 `xml:"r,attr"`
-}
-
-type Line struct {
-	X1 float64 `xml:"x1,attr"`
-	Y1 float64 `xml:"y1,attr"`
-	X2 float64 `xml:"x2,attr"`
-	Y2 float64 `xml:"y2,attr"`
-}
-
-// func TestSecondHandAtMidnight(t *testing.T) {
-// 	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
-
-// 	want := clockface.Point{X: 150, Y: 150 - 90}
-// 	got := clockface.SecondHand(tm)
-
-// 	if got != want {
-// 		t.Errorf("Got %v, want %v", got, want)
-// 	}
-// }
-
-// func TestSecondHandAt30Seconds(t *testing.T) {
-// 	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
-
-// 	want := clockface.Point{X: 150, Y: 150 + 90}
-// 	got := clockface.secondHand(tm)
-
-// 	if got != want {
-// 		t.Errorf("Got %v, want %v", got, want)
-// 	}
-// }
-
-func TestSVGWriterSecondHand(t *testing.T) {
+func TestSecondesInRadians(t *testing.T) {
 	cases := []struct {
-		time time.Time
-		line Line
+		time  time.Time
+		angle float64
 	}{
-		{simpleTime(0, 0, 0), Line{150, 150, 150, 60}},
-		{simpleTime(0, 0, 30), Line{150, 150, 150, 240}},
+		{simpleTime(0, 0, 30), math.Pi},
+		{simpleTime(0, 0, 0), 0},
+		{simpleTime(0, 0, 45), math.Pi / 2 * 3},
+		{simpleTime(0, 0, 7), math.Pi / 30 * 7},
 	}
 
 	for _, c := range cases {
 		t.Run(testName(c.time), func(t *testing.T) {
+			got := secondsInRadians(c.time)
 
-			b := bytes.Buffer{}
-			clockface.SVGWriter(&b, c.time)
-			svg := SVG{}
-			xml.Unmarshal(b.Bytes(), &svg)
-
-			if !containsLine(c.line, svg.Line) {
-
-				t.Errorf("Expected to find the second hand line %+v, in the SVG lines %+v", c.line, svg.Line)
+			if got != c.angle {
+				t.Fatalf("Wanted %v, but got %v", c.angle, got)
 			}
 		})
 	}
 }
 
-func TestSVGWriterMinuteHand(t *testing.T) {
+func TestMinutesInRadians(t *testing.T) {
 	cases := []struct {
-		time time.Time
-		line Line
+		time  time.Time
+		angle float64
 	}{
-		{simpleTime(0, 0, 0), Line{150, 150, 150, 60}},
+		{simpleTime(0, 30, 0), math.Pi},
+		{simpleTime(0, 0, 7), 7 * (math.Pi / (30 * 60))},
 	}
 
 	for _, c := range cases {
 		t.Run(testName(c.time), func(t *testing.T) {
-			b := bytes.Buffer{}
-			clockface.SVGWriter(&b, c.time)
+			got := minutesInRadians(c.time)
 
-			svg := SVG{}
-			xml.Unmarshal(b.Bytes(), &svg)
-
-			if !containsLine(c.line, svg.Line) {
-
-				t.Errorf("Expected to find the minute hand line %+v, in the SVG lines %+v", c.line, svg.Line)
+			if got != c.angle {
+				t.Fatalf("Wanted %v, but got %v", c.angle, got)
 			}
 		})
 	}
 }
 
-func TestSVGWriterHourHand(t *testing.T) {
+func TestHoursInRadians(t *testing.T) {
 	cases := []struct {
-		time time.Time
-		line Line
+		time  time.Time
+		angle float64
 	}{
-		{simpleTime(6, 0, 0), Line{150, 150, 150, 200}},
+		{simpleTime(6, 0, 0), math.Pi},
+		{simpleTime(0, 0, 0), 0},
+		{simpleTime(21, 0, 0), math.Pi * 1.5},
+		{simpleTime(0, 1, 30), math.Pi / ((6 * 60 * 60) / 90)},
 	}
 
 	for _, c := range cases {
 		t.Run(testName(c.time), func(t *testing.T) {
-			b := bytes.Buffer{}
-			clockface.SVGWriter(&b, c.time)
+			got := hoursInRadians(c.time)
 
-			svg := SVG{}
-			xml.Unmarshal(b.Bytes(), &svg)
-
-			if !containsLine(c.line, svg.Line) {
-				t.Errorf("Expected to find the hour hand line %+v, in the SVG lines %+v", c.line, svg.Line)
+			if !roughlyEqualFloat64(c.angle, got) {
+				t.Fatalf("Wanted %v, but got %v", c.angle, got)
 			}
 		})
 	}
 }
 
-func containsLine(c Line, s []Line) bool {
-	for _, l := range s {
-		if l == c {
-			return true
-		}
+func TestMinuteHandPoint(t *testing.T) {
+	cases := []struct {
+		time  time.Time
+		point Point
+	}{
+		{simpleTime(0, 30, 0), Point{0, -1}},
+		{simpleTime(0, 45, 0), Point{-1, 0}},
 	}
-	return false
+
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			got := minuteHandPoint(c.time)
+
+			if !roughlyEqualPoint(got, c.point) {
+				t.Fatalf("Wanted %v, but got %v", c.point, got)
+			}
+		})
+	}
+}
+
+func TestHourHandPoint(t *testing.T) {
+	cases := []struct {
+		time  time.Time
+		point Point
+	}{
+		{simpleTime(6, 0, 0), Point{0, -1}},
+		{simpleTime(21, 0, 0), Point{-1, 0}},
+	}
+
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			got := hourHandPoint(c.time)
+
+			if !roughlyEqualPoint(got, c.point) {
+				t.Fatalf("Wanted %v, but got %v", c.point, got)
+			}
+		})
+	}
+}
+
+func TestSecondHandVector(t *testing.T) {
+	cases := []struct {
+		time  time.Time
+		point Point
+	}{
+		{simpleTime(0, 0, 30), Point{0, -1}},
+		{simpleTime(0, 0, 45), Point{-1, 0}},
+	}
+
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			got := secondHandPoint(c.time)
+
+			if !roughlyEqualPoint(got, c.point) {
+				t.Fatalf("Wanted %v, but got %v", c.point, got)
+			}
+		})
+	}
+}
+
+func roughlyEqualFloat64(a, b float64) bool {
+	const equalityThreshold = 1e-7
+	return math.Abs(a-b) < equalityThreshold
+}
+
+func roughlyEqualPoint(a, b Point) bool {
+	return roughlyEqualFloat64(a.X, b.X) && roughlyEqualFloat64(a.Y, b.Y)
+}
+
+func simpleTime(hours, minutes, seconds int) time.Time {
+	return time.Date(312, time.October, 28, hours, minutes, seconds, 0, time.UTC)
 }
 
 func testName(t time.Time) string {
 	return t.Format("15:04:05")
-}
-func simpleTime(hours, minutes, seconds int) time.Time {
-	return time.Date(312, time.October, 28, hours, minutes, seconds, 0, time.UTC)
 }
