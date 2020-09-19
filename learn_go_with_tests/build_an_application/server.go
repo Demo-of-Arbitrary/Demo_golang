1899e335a08a6e4267b017b68e6bac071f4b274e
George Xie <georgexie@George.local>
2020-09-19 09:29:49 +0800

    feat: first step with minor refactor

--- learn_go_with_tests/build_an_application/server.go
+++ learn_go_with_tests/build_an_application/server.go
@@ -3,9 +3,23 @@ package server
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 // PlayerServer is
 func PlayerServer(response http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(response, "20")
+	player := strings.TrimPrefix(request.URL.Path, "/players/")
+	fmt.Fprintf(response, GetPlayerScore(player))
+}
+
+// GetPlayerScore is
+func GetPlayerScore(player string) string {
+	if player == "Pepper" {
+		return "20"
+	}
+
+	if player == "Floyd" {
+		return "10"
+	}
+	return ""
 }
--- learn_go_with_tests/build_an_application/server_test.go
+++ learn_go_with_tests/build_an_application/server_test.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -8,16 +9,31 @@ import (
 
 func TestGETPlayers(t *testing.T) {
 	t.Run("Returns Pepper's score", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodGet, "/players/Pepper", nil)
+		request := newGetScoreRequest("Pepper")
 		response := httptest.NewRecorder()
 
 		PlayerServer(response, request)
 
-		got := response.Body.String()
-		want := "20"
-
-		if got != want {
-			t.Errorf("Got %q, want %q", got, want)
-		}
+		assertResponseBody(t, response.Body.String(), "20")
 	})
+	t.Run("Returns Floyd's score", func(t *testing.T) {
+		request := newGetScoreRequest("Floyd")
+		response := httptest.NewRecorder()
+
+		PlayerServer(response, request)
+
+		assertResponseBody(t, response.Body.String(), "10")
+	})
+}
+
+func newGetScoreRequest(name string) *http.Request {
+	request, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/players/%s", name), nil)
+	return request
+}
+
+func assertResponseBody(t *testing.T, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("response boyd is wrong, got %q, want %q", got, want)
+	}
 }
