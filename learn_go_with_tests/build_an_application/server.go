44228690f099e7c4e06cfac84093200a8507068f
George Xie <georgexie@George.local>
2020-09-19 11:13:01 +0800

    refact: extract code

--- learn_go_with_tests/build_an_application/server.go
+++ learn_go_with_tests/build_an_application/server.go
@@ -14,11 +14,23 @@ type PlayerServer struct {
 	store PlayerStore
 }
 
-func (p *PlayerServer) ServeHTTP(response http.ResponseWriter, request *http.Request) {
-	if request.Method == http.MethodPost {
-		response.WriteHeader(http.StatusAccepted)
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		showScore(w, r, p)
+	case http.MethodPost:
+		processWins(w, r)
+	}
+}
+
+func processWins(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		w.WriteHeader(http.StatusAccepted)
 		return
 	}
+}
+
+func showScore(response http.ResponseWriter, request *http.Request, p *PlayerServer) {
 	player := strings.TrimPrefix(request.URL.Path, "/players/")
 	score := p.store.GetPlayerScore(player)
 	if score == 0 {
