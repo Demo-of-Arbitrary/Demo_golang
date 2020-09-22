07558ead573a555c175d7c7c65d6b68ee16387f0
George Xie <georgexie@George.local>
2020-09-22 14:23:41 +0800

    feat: implment and test POST

--- learn_go_with_tests/build_an_application/server.go
+++ learn_go_with_tests/build_an_application/server.go
@@ -8,6 +8,7 @@ import (
 
 type PlayerStore interface {
 	GetPlayerScore(string) int
+	RecordWin(name string)
 }
 
 type PlayerServer struct {
@@ -17,20 +18,19 @@ type PlayerServer struct {
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
-		showScore(w, r, p)
+		p.showScore(w, r)
 	case http.MethodPost:
-		processWins(w, r)
+		p.processWins(w, r)
 	}
 }
 
-func processWins(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodPost {
-		w.WriteHeader(http.StatusAccepted)
-		return
-	}
+func (p *PlayerServer) processWins(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+	p.store.RecordWin(player)
+	w.WriteHeader(http.StatusAccepted)
 }
 
-func showScore(response http.ResponseWriter, request *http.Request, p *PlayerServer) {
+func (p *PlayerServer) showScore(response http.ResponseWriter, request *http.Request) {
 	player := strings.TrimPrefix(request.URL.Path, "/players/")
 	score := p.store.GetPlayerScore(player)
 	if score == 0 {
