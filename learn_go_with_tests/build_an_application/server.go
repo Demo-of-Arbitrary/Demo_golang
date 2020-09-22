664591c78c422ef1c8dea7a2ffaa813eb4df6dd0
George Xie <georgexie@George.local>
2020-09-22 14:31:10 +0800

    refact: minor extract

--- learn_go_with_tests/build_an_application/server.go
+++ learn_go_with_tests/build_an_application/server.go
@@ -16,22 +16,21 @@ type PlayerServer struct {
 }
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
 	switch r.Method {
 	case http.MethodGet:
-		p.showScore(w, r)
+		p.showScore(w, player)
 	case http.MethodPost:
-		p.processWins(w, r)
+		p.processWins(w, player)
 	}
 }
 
-func (p *PlayerServer) processWins(w http.ResponseWriter, r *http.Request) {
-	player := strings.TrimPrefix(r.URL.Path, "/players/")
+func (p *PlayerServer) processWins(w http.ResponseWriter, player string) {
 	p.store.RecordWin(player)
 	w.WriteHeader(http.StatusAccepted)
 }
 
-func (p *PlayerServer) showScore(response http.ResponseWriter, request *http.Request) {
-	player := strings.TrimPrefix(request.URL.Path, "/players/")
+func (p *PlayerServer) showScore(response http.ResponseWriter, player string) {
 	score := p.store.GetPlayerScore(player)
 	if score == 0 {
 		response.WriteHeader(http.StatusNotFound)
