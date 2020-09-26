fc8e55166174b789ede59f705f579c456b841245
George Xie <georgexie@George.local>
2020-09-26 21:36:31 +0800

    feat: use io.File to implement persist

--- learn_go_with_tests/build_an_application/FileSystemStore_test.go
+++ learn_go_with_tests/build_an_application/FileSystemStore_test.go
@@ -1,16 +1,20 @@
 package main
 
 import (
-	"strings"
+	"io"
+	"io/ioutil"
+	"os"
 	"testing"
 )
 
 func TestFileSystemStore(t *testing.T) {
 	t.Run("/league from a reader", func(t *testing.T) {
-		database := strings.NewReader(`[
+		database, cleanDatabase := createTempFile(t, `[
 			{"Name": "Cleo", "Wins": 10},
 			{"Name": "Chris", "Wins": 33}]
 			`)
+		defer cleanDatabase()
+
 		store := FileSystemPlayerStore{database}
 
 		got := store.GetLeague()
@@ -24,10 +28,12 @@ func TestFileSystemStore(t *testing.T) {
 	})
 
 	t.Run("get player score", func(t *testing.T) {
-		database := strings.NewReader(`[
+		database, cleanDatabase := createTempFile(t, `[
 			{"Name": "Cleo", "Wins": 10},
 			{"Name": "Chris", "Wins": 33}]
 			`)
+		defer cleanDatabase()
+
 		store := FileSystemPlayerStore{database}
 
 		got := store.GetPlayerScore("Chris")
@@ -41,4 +47,21 @@ func assertScoreEqual(t *testing.T, got, want int) {
 	if got != want {
 		t.Errorf("got %d want %d", got, want)
 	}
+}
+
+func createTempFile(t *testing.T, initialData string) (io.ReadWriteSeeker, func()) {
+	t.Helper()
+	tmpfile, err := ioutil.TempFile("", "db")
+
+	if err != nil {
+		t.Fatalf("could not create temp file: %v", err)
+	}
+
+	tmpfile.Write([]byte(initialData))
+
+	removeFile := func() {
+		tmpfile.Close()
+		os.Remove(tmpfile.Name())
+	}
+	return tmpfile, removeFile
 }
