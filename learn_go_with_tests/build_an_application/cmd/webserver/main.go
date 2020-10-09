2d5f9d1e6f1328b66b64cf36b5c4988e01ed9ae9
George Xie <georgexie@George.local>
2020-10-09 14:37:23 +0800

    refact: rearrange file structure

--- learn_go_with_tests/build_an_application/cmd/webserver/main.go
+++ learn_go_with_tests/build_an_application/cmd/webserver/main.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	poker "github.com/Demo_golang/learn_go_with_tests/build_an_application"
 )
 
 const dbFileName = "game.db.json"
@@ -15,13 +17,13 @@ func main() {
 		log.Fatalf("problem opening database: %s %v", dbFileName, err)
 	}
 
-	store, err := NewFileSystemPlayerStore(database)
+	store, err := poker.NewFileSystemPlayerStore(database)
 
 	if err != nil {
 		log.Fatalf("problem creating file system player store: %v", err)
 	}
 
-	server := NewPlayerServer(store)
+	server := poker.NewPlayerServer(store)
 
 	if err := http.ListenAndServe(":5000", server); err != nil {
 		log.Fatalf("could not listen on port 5000 %v", err)
