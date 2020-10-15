ccdf199d30b5206845bbc298736a6e54f6bb43e6
George Xie <georgexie@George.local>
2020-10-15 10:59:44 +0800

    feat: schedule alert for basic 5 player

--- learn_go_with_tests/build_an_application/cli.go
+++ learn_go_with_tests/build_an_application/cli.go
@@ -4,18 +4,29 @@ import (
 	"bufio"
 	"io"
 	"strings"
+	"time"
 )
 
 type CLI struct {
 	playerStore PlayerStore
 	in          *bufio.Scanner
+	alerter     BlindAlerter
+}
+type BlindAlerter interface {
+	ScheduleAlertAt(duration time.Duration, amount int)
 }
 
-func NewCLI(store PlayerStore, in io.Reader) *CLI {
-	return &CLI{playerStore: store, in: bufio.NewScanner(in)}
+func NewCLI(store PlayerStore, in io.Reader, alerter BlindAlerter) *CLI {
+	return &CLI{playerStore: store, in: bufio.NewScanner(in), alerter: alerter}
 }
 
 func (c *CLI) PlayPoker() {
+	blinds := []int{100, 200, 300, 400, 500, 600, 800, 1000, 2000, 4000, 8000}
+	blindTime := 0 * time.Second
+	for _, blind := range blinds {
+		c.alerter.ScheduleAlertAt(blindTime, blind)
+		blindTime = blindTime + 10*time.Minute
+	}
 	userInput := c.readLine()
 	c.playerStore.RecordWin(extractWinner(userInput))
 }
